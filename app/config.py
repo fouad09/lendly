82eef60a05e9c206e676b0b73b61912088002049
Fouad Kouidmir <fouadkouidmir@gmail.com>
2025-07-10 21:10:18 +0400

    first push

--- app/config.py
+++ app/config.py
@@ -2,6 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    DATABASE_URL: str
     SECRET_KEY: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
     REFRESH_TOKEN_EXPIRE_DAYS: int
